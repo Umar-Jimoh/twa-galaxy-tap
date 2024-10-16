56984294c3967a826399b2ea98a85cc7699ab3ed
Umar Jimoh <umar@Umars-MacBook-Air.local>
2024-10-16 15:40:02 +0100

    Design the UI

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <title>Galaxy Tap</title>
+</head>
+<body>
+    <div class="container">
+        <main class="main">
+            <p class="username" id="username">john</p>
+            <span class="points" id="points">0</span>
+            <div class="image-container">
+                <img class="image" id="image" src="./images/button.jpg" alt="space image">
+            </div>
+            <div class="footer">
+                <div class="progress-bar" id="progress-bar">
+                    <span class="progress" id="progress"></span>
+                </div>
+                <button class="invite_button">
+                    invite Friend
+                </button>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
