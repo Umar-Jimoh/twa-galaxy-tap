69a45c63fe05a327dc2841a83238de3cf3d96009
Umar Jimoh <umar@Umars-MacBook-Air.local>
2024-10-23 17:51:56 +0100

    Implement addUser to DB

--- config/dbConfig.php
+++ config/dbConfig.php
@@ -1,5 +1,5 @@
 <?php
-require '../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -12,6 +12,7 @@ class DBConnection
     private $password;
     private $dbname;
     protected $conn;
+    private static $connInfo;
 
     private function __construct()
     {
@@ -19,13 +20,14 @@ class DBConnection
         $this->username = $_ENV["DB_USERNAME"];
         $this->password = $_ENV["DB_PASSWORD"];
         $this->dbname = $_ENV["DB_NAME"];
+        self::$connInfo = ['status' => null, 'error' => null];
 
         try {
             $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
+            self::$connInfo = ['status' => 'success', 'error' => null];
         } catch (PDOException $e) {
-            echo "Connection failed" . $e->getMessage();
+            self::$connInfo = ['status' => 'failed', 'error' => $e->getMessage()];
         }
     }
 
@@ -41,6 +43,11 @@ class DBConnection
     public function getConnection()
     {
         return $this->conn;
+    }
+
+    public static function getConnectionInfo()
+    {
+        return self::$connInfo;
     }
 
     function __clone() {}
