e67e1fe06bd90d6ddb048190a61979ab84324934
Umar Jimoh <umarjimoh@hotmail.com>
2024-10-23 17:51:56 +0100

    Implement addUser to DB

--- src/controller/handleWebAppData.php
+++ src/controller/handleWebAppData.php
@@ -1,4 +1,6 @@
 <?php
+require '../model/database.php';
+header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $payload = file_get_contents('php://input');
@@ -8,5 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     file_put_contents('../../logs/webhook.log', print_r($output, true), FILE_APPEND);
 
-    echo json_encode($output['user']);
+    // Database
+    try {
+        $db = new DB();
+        $user = json_decode($output['user']);
+        $db->addUser($user);
+    } catch (PDOException $e) {
+        echo json_encode(['status' => 'failed', 'error' => $e->getMessage()]);
+    }
 }
---
+++ src/model/database.php
@@ -0,0 +1,39 @@
+<?php
+require '../../config/dbconfig.php';
+
+class DB extends DBConnection
+{
+    public function __construct() {}
+
+    public function addUser($user)
+    {
+        $dbInstance = self::GetInstance();
+        $conn = $dbInstance->getConnection();
+        $connInfo = self::getConnectionInfo();
+
+        if ($connInfo['status'] === 'success') {
+            try {
+                $sql = "INSERT INTO Users(id, username, first_name, language_code) VALUES (:id, :username, :first_name, :language_code)";
+                $stmt = $conn->prepare($sql);
+
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+                $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+                $stmt->bindParam(':first_name', $first_name, PDO::PARAM_STR);
+                $stmt->bindParam(':language_code', $language_code, PDO::PARAM_STR);
+
+                // Set value from the user object
+                $id = $user->id;
+                $username = $user->username;
+                $first_name = $user->first_name;
+                $language_code = $user->language_code;
+
+                // $stmt->execute();
+                echo json_encode(['status' => 'success', 'data' => ($user)]);
+            } catch (PDOException $e) {
+                echo json_encode(['status' => 'failed', 'error' => $e->getMessage()]);
+            }
+        } else {
+            echo json_encode(['status' => 'failed', 'error' => $connInfo['error']]);
+        }
+    }
+}
